67102f9c5baa3cbc8928473e828f1c8e4616dbc1
Eider Diaz <eiderdiazmoraila@hotmail.com>
2022-03-27 21:23:39 -0700

    added a new route (project/<id>) and template

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ app = Flask(__name__)
 def show_projects():
 	return render_template("index.html")
 
+#generamos una nueva ruta /project/<project_id> que cargara diferentes proyectos
+@app.route("/project/<project_id>")
+def show_tasks(project_id):
+	return render_template("project-tasks.html", project_id=project_id)
+
+
+
 #correr la app en el localhost puerto 3000 (127.0.0.1:3000)
 app.run(debug=True, host="127.0.0.1", port=3000)
