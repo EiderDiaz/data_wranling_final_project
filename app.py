4e23615b08577b8e947ad4983308b78e16311163
Eider Diaz <eiderdiazmoraila@hotmail.com>
2022-03-27 23:06:07 -0700

    data inserted into db, query in app and render in index template

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+    project_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(length=50))
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    task_id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.project_id'))
+    description = db.Column(db.String(length=50))
+    project = db.relationship("Project")
+
+
+
 #ruta / (root o home)
 @app.route("/")
-
 #funcion que renderea el archivo html 
 def show_projects():
-	return render_template("index.html")
+	return render_template("index.html", projects=Project.query.all())
 
 #generamos una nueva ruta /project/<project_id> que cargara diferentes proyectos
 @app.route("/project/<project_id>")
