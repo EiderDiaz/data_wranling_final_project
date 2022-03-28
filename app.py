e43c6e8709951da01fd4f43eebc9940452528a35
Eider Diaz <eiderdiazmoraila@hotmail.com>
2022-03-27 21:40:43 -0700

    created routes for projects and tasks

--- app.py
+++ app.py
@@ -13,7 +13,16 @@ def show_projects():
 def show_tasks(project_id):
 	return render_template("project-tasks.html", project_id=project_id)
 
-
+#ruta para añadir proyectos
+@app.route("/add/project", methods=['POST'])
+def add_project():
+	#Add project
+	return "Project added sucessfully"
+#ruta tu ya sabes pa que
+@app.route("/add/task/<project_id>", methods=['POST'])
+def add_task(project_id):
+	#Add task
+	return "Task added successfully"
 
 #correr la app en el localhost puerto 3000 (127.0.0.1:3000)
 app.run(debug=True, host="127.0.0.1", port=3000)
