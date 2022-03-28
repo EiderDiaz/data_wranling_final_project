adf0335bfdcd5396d33cb395da3798af64460c16
Eider Diaz <eiderdiazmoraila@hotmail.com>
2022-03-28 00:00:26 -0700

    insert data into tables using form data from template and  manipulating with the app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+#crear un secret para mantener las escrituras a la db seguras
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 db = SQLAlchemy(app)
 
@@ -31,19 +34,37 @@ def show_projects():
 #generamos una nueva ruta /project/<project_id> que cargara diferentes proyectos
 @app.route("/project/<project_id>")
 def show_tasks(project_id):
-	return render_template("project-tasks.html",
-    project=Project.query.filter_by(project_id=project_id).first(),
+	return render_template("project-tasks.html", 
+		project=Project.query.filter_by(project_id=project_id).first(),
 		tasks=Task.query.filter_by(project_id=project_id).all())
 #ruta para añadir proyectos
 @app.route("/add/project", methods=['POST'])
 def add_project():
 	#Add project
-	return "Project added sucessfully"
+    #el nombre del formulario que revisamos tiene que coincidir con el del template
+	if not request.form['project-title']:
+        #mandamos alerta en caso que no tenga nada
+		flash("Enter a title for your new project", "red")
+	else:
+        #creamos un nuevo projecto y lo insertamos en la tabla 
+		project = Project(title=request.form['project-title'])
+		db.session.add(project)
+		db.session.commit()
+		flash("Project added successfully", "green")
+	return redirect(url_for('show_projects'))
+
 #ruta tu ya sabes pa que
 @app.route("/add/task/<project_id>", methods=['POST'])
 def add_task(project_id):
 	#Add task
-	return "Task added successfully"
+	if not request.form['task-description']:
+		flash("Enter a description for your new task", "red")
+	else:
+		task = Task(description=request.form['task-description'], project_id=project_id)
+		db.session.add(task)
+		db.session.commit()
+		flash("Task added successfully", "green")
+	return redirect(url_for('show_tasks', project_id=project_id))
 
 #correr la app en el localhost puerto 3000 (127.0.0.1:3000)
 app.run(debug=True, host="127.0.0.1", port=3000)
