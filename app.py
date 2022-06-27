7c2c465bad04a55b2d64a7fa650bba3a648615f5
Eider Diaz <eiderdiazmoraila@hotmail.com>
2022-06-27 17:48:29 -0500

    finished funtionalities

--- app.py
+++ app.py
@@ -67,5 +67,27 @@ def add_task(project_id):
 		db.session.commit()
 		flash("Task added successfully", "green")
 	return redirect(url_for('show_tasks', project_id=project_id))
+
+
+@app.route("/delete/task/<task_id>", methods=['POST'])
+def delete_task(task_id):
+	#seleccionamos la task a borrar
+	pending_delete_task = Task.query.filter_by(task_id=task_id).first()
+	#obtenemos el proyecto al que pertenece la task y lo guardamos en original_project_id
+	original_project_id = pending_delete_task.project.project_id
+	#hacemos el borrado en la db 
+	db.session.delete(pending_delete_task)
+	db.session.commit()
+	#nos vamos a show_task, la misma url donde estamos (y como necesita el project_id le mandamos original_project_id )
+	return redirect(url_for('show_tasks', project_id=original_project_id))
+
+@app.route("/delete/project/<project_id>", methods=['POST'])
+def delete_project(project_id):
+	pending_delete_project = Project.query.filter_by(project_id=project_id).first()
+	db.session.delete(pending_delete_project)
+	db.session.commit()
+	return redirect(url_for('show_projects'))
+
+	
 #correr la app en el localhost puerto 3000 (127.0.0.1:3000)
 app.run(debug=True, host="127.0.0.1", port=3000)
